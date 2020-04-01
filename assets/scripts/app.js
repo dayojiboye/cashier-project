1f3366b241cdb12b1dace0900ebd46d3a4251e3e
Dayo Jiboye <dayojiboye2@gmail.com>
2020-04-01 20:40:51 +0100

    A PROJECT THAT SHOWS ITEMS AND THEIR CORRESPONDING PRICES WITH THE TOTAL PRICE UPDATED ONCE YOU ADD OR DELETE AN ITEM

---
+++ assets/scripts/app.js
@@ -0,0 +1,105 @@
+const itemInput = document.getElementById('item1');
+const priceInput = document.getElementById('price1');
+const totalPrice = document.getElementById('t-price');
+const addBtnElement = document.getElementById('add-btn');
+const deleteBtnElement = document.getElementById('delete-button');
+const alertBoxElement = document.querySelector('.alert-box');
+const tableHeader = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+const myItems = [];
+
+const hideAlert = () => {
+  setTimeout(() => {
+    alertBoxElement.classList.remove('visible');
+  }, 2000);
+};
+
+const clearInput = () => {
+  itemInput.value = '';
+  priceInput.value = '';
+};
+
+const showTableHeader = () => {
+  tableHeader.classList.add('visible');
+};
+
+const hideTableHeader = () => {
+  if (myItems.length === 0) {
+    tableHeader.classList.remove('visible');
+  }
+};
+
+const getTotalPrice = () => {
+  const tPrice = myItems.reduce(
+    (prevValue, curValue) => (prevValue += curValue.price),
+    0
+  );
+  return (totalPrice.textContent = `$${tPrice}`);
+};
+
+const deleteItemHandler = function(id) {
+  if (this.checked === false) {
+    return;
+  }
+
+  this.parentNode.parentNode.remove();
+  let itemIndex = 0;
+  for (const entry of myItems) {
+    if (entry.id === id) {
+      break;
+    }
+    itemIndex++;
+  }
+  myItems.splice(itemIndex, 1);
+  hideTableHeader();
+  getTotalPrice();
+};
+
+const getNewItem = (id, item, price) => {
+  const tableRow = document.createElement('tr');
+  tableRow.className = 'table-pointer';
+  tableRow.innerHTML = `
+      <td>
+          <input type="checkbox" name="checkbx" id="checkbx" value="" />
+      </td>
+      <td>${item}</td>
+      <td>$${price}</td>`;
+
+  const checkBox = tableRow.querySelector('#checkbx');
+
+  checkBox.addEventListener('click', function() {
+    deleteBtnElement.addEventListener(
+      'click',
+      deleteItemHandler.bind(this, id)
+    );
+  });
+
+  tableRow.addEventListener('click', () => {
+    checkBox.click();
+  });
+
+  tbody.append(tableRow);
+};
+
+const addItemHandler = () => {
+  if (itemInput.value.trim() === '' || priceInput.value === '') {
+    alertBoxElement.classList.add('visible');
+    hideAlert();
+    return;
+  }
+
+  const itemObj = {
+    id: Math.random(),
+    item: itemInput.value.trim().toUpperCase(),
+    price: parseInt(priceInput.value)
+  };
+
+  showTableHeader();
+  getNewItem(itemObj.id, itemObj.item, itemObj.price);
+  myItems.push(itemObj);
+  getTotalPrice();
+  clearInput();
+};
+
+addBtnElement.addEventListener('click', addItemHandler);
